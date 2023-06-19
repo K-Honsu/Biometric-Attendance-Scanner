4d5da8eedc029b732f320816b7a1244fc6cf3045
P-Horus <feranmia51@gmail.com>
2023-06-19 17:54:57 +0100

    fixed swagger url

--- biometrics/urls.py
+++ biometrics/urls.py
@@ -1,28 +1,54 @@
+# from django.contrib import admin
+# from django.urls import path, include
+# from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+# schema_view = swagger_get_schema_view(
+#     openapi.Info(
+#         title="Biometrics API",
+#         default_version="1.0.0",
+#         description="API DOCUMENTATION OF APP"
+#     ),
+#     public=True
+# )
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/v1/', include([
+#          path('users/', include('users.urls')),
+#          path('auth/', include('djoser.urls')),
+#          path('auth/', include('djoser.urls.jwt')),
+#          path('docs/', schema_view.with_ui('swagger',
+#              cache_timeout=0), name='swagger-schema'),
+#     ])),
+    # path('users/', include('users.urls')),
+    # path('school/', include('school.urls')),
+    # path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls.jwt')),
+# ]
+
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
-from drf_yasg.views import get_schema_view as swagger_get_schema_view
+from drf_yasg.views import get_schema_view
 
-schema_view = swagger_get_schema_view(
+schema_view = get_schema_view(
     openapi.Info(
         title="Biometrics API",
         default_version="1.0.0",
         description="API DOCUMENTATION OF APP"
     ),
-    public=True
+    public=True,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include([
-         path('users/', include('users.urls')),
-         path('auth/', include('djoser.urls')),
-         path('auth/', include('djoser.urls.jwt')),
-         path('docs/', schema_view.with_ui('swagger',
-             cache_timeout=0), name='swagger-schema'),
+        path('users/', include('users.urls')),
+        path('auth/', include('djoser.urls')),
+        path('auth/', include('djoser.urls.jwt')),
+        path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
+        path('docs/swagger.json', schema_view.without_ui(cache_timeout=0), name='swagger-json'),
     ])),
-    # path('users/', include('users.urls')),
-    path('school/', include('school.urls')),
-    # path('auth/', include('djoser.urls')),
-    # path('auth/', include('djoser.urls.jwt')),
 ]
+

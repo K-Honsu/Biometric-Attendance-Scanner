0837769dbf5f83ac5ddea4c4f60aa4234bf61e5f
P-Horus <feranmia51@gmail.com>
2023-06-19 18:04:05 +0100

    fixed swagger url

--- biometrics/settings.py
+++ biometrics/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["biometric-oqav.onrender.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["biometric-oqav.onrender.com", "127.0.0.0", "localhost"]
 
 
 # Application definition

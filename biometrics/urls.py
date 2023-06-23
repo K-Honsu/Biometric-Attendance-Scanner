fc046633279ad414c4782a56d202c82bc20629bb
P-Horus <feranmia51@gmail.com>
2023-06-23 15:55:54 +0100

    fixed errors

--- biometrics/urls.py
+++ biometrics/urls.py
@@ -21,16 +21,18 @@
 #          path('docs/', schema_view.with_ui('swagger',
 #              cache_timeout=0), name='swagger-schema'),
 #     ])),
-    # path('users/', include('users.urls')),
-    # path('school/', include('school.urls')),
-    # path('auth/', include('djoser.urls')),
-    # path('auth/', include('djoser.urls.jwt')),
+# path('users/', include('users.urls')),
+# path('school/', include('school.urls')),
+# path('auth/', include('djoser.urls')),
+# path('auth/', include('djoser.urls.jwt')),
 # ]
 
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -47,10 +49,15 @@ urlpatterns = [
         path('users/', include('users.urls')),
         path('auth/', include('djoser.urls')),
         path('auth/', include('djoser.urls.jwt')),
-        path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
-        path('docs/swagger.json', schema_view.without_ui(cache_timeout=0), name='swagger-json'),
+        path('docs/', schema_view.with_ui('swagger',
+             cache_timeout=0), name='swagger-schema'),
+        path('docs/swagger.json', schema_view.without_ui(cache_timeout=0),
+             name='swagger-json'),
     ])),
     path('users/', include('users.urls')),
     path('school/', include('school.urls')),
 ]
 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
